828eb699c8204c50bd39eb92a06de362ab66072a
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-27 01:12:21 -0300

    Closes #6 "criada as funcionalidades de pesquisas"

---
+++ app_netdrinks/lib/widgets/cocktail_card_widget.dart
@@ -0,0 +1,32 @@
+import 'package:app_netdrinks/models/cocktail.dart';
+import 'package:flutter/material.dart';
+
+class CocktailCard extends StatelessWidget {
+  final Cocktail cocktail;
+
+  const CocktailCard({super.key, required this.cocktail});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, 4),
+            blurRadius: 8,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: Image.network(
+          cocktail.thumbnailUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
