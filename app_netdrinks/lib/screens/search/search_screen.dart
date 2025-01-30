fce4325589c7d1fe3283fbbb68fd1d0a2572daf8
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-30 10:21:21 -0300

    refatora tela de busca para usar StatefulWidget, adiciona gerenciamento de foco e melhora a apresentação dos resultados

--- app_netdrinks/lib/screens/search/search_screen.dart
+++ app_netdrinks/lib/screens/search/search_screen.dart
@@ -2,11 +2,31 @@ import 'package:app_netdrinks/controller/search_controller.dart' as netdrink;
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
+  SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
   final controller = Get.put(netdrink.SearchController());
   final searchController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
-  SearchScreen({super.key});
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    searchController.dispose();
+    super.dispose();
+  }
+
+  void _handleSearch() {
+    _focusNode.unfocus(); // Fecha o teclado
+    controller.searchByFirstLetter(searchController.text);
+    final searchText = searchController.text.trim(); // Remove espaços em branco
+    controller.searchByFirstLetter(searchText);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,12 +46,12 @@ class SearchScreen extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: TextField(
               controller: searchController,
+              focusNode: _focusNode,
               decoration: InputDecoration(
                 hintText: 'Buscar por primeira letra...',
                 suffixIcon: IconButton(
                   icon: Icon(Icons.search),
-                  onPressed: () =>
-                      controller.searchByFirstLetter(searchController.text),
+                  onPressed: _handleSearch,
                 ),
               ),
             ),
@@ -40,14 +60,32 @@ class SearchScreen extends StatelessWidget {
               ? CircularProgressIndicator()
               : Expanded(
                   child: ListView.builder(
+                    padding: const EdgeInsets.all(8.0), // Padding na lista
                     itemCount: controller.searchResults.length,
                     itemBuilder: (context, index) {
                       final cocktail = controller.searchResults[index];
-                      return ListTile(
-                        leading: Image.network(cocktail.imageUrl),
-                        title: Text(cocktail.name),
-                        onTap: () => Get.toNamed('/cocktail-detail',
-                            arguments: cocktail),
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8.0), // Espaço entre itens
+                        child: ListTile(
+                          leading: Container(
+                            width: 80, // Aumentar largura da imagem
+                            height: 80, // Aumentar altura da imagem
+                            child: Image.network(
+                              cocktail.imageUrl,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          title: Text(
+                            cocktail.name,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onTap: () => Get.toNamed('/cocktail-detail',
+                              arguments: cocktail),
+                        ),
                       );
                     },
                   ),
