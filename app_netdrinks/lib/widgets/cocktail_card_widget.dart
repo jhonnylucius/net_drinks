272df12f766b451533be97945e8320b7f71828fe
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-02-01 12:58:56 -0300

    adiciona dimensões fixas ao card de coquetel e ajusta layout para melhor apresentação

--- app_netdrinks/lib/widgets/cocktail_card_widget.dart
+++ app_netdrinks/lib/widgets/cocktail_card_widget.dart
@@ -6,6 +6,8 @@ import 'package:get/get.dart';
 class CocktailCard extends StatelessWidget {
   final Cocktail cocktail;
   final String user;
+  final double cardWidth = 300; // Largura fixa do card
+  final double cardHeight = 200; // Altura fixa do card
 
   CocktailCard({super.key, required this.cocktail, required this.user});
 
@@ -14,6 +16,8 @@ class CocktailCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: cardWidth,
+      height: cardHeight,
       margin: const EdgeInsets.symmetric(horizontal: 8),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16),
@@ -25,29 +29,40 @@ class CocktailCard extends StatelessWidget {
           ),
         ],
       ),
-      child: Stack(
+      child: Row(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(16),
-            child: Image.network(
-              cocktail.thumbnailUrl,
-              fit: BoxFit.cover,
+          Expanded(
+            flex: 4,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image.network(
+                cocktail.thumbnailUrl,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          Positioned(
-            top: 8,
-            right: 8,
-            child: Obx(() {
-              return IconButton(
-                icon: Icon(
-                  controller.isFavorite(cocktail.idDrink)
-                      ? Icons.favorite
-                      : Icons.favorite_border,
-                  color: Colors.red,
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Obx(() {
+                    return IconButton(
+                      icon: Icon(
+                        controller.isFavorite(cocktail.idDrink)
+                            ? Icons.favorite
+                            : Icons.favorite_border,
+                        color: Colors.red,
+                      ),
+                      onPressed: () =>
+                          controller.toggleFavorite(cocktail.idDrink),
+                    );
+                  }),
                 ),
-                onPressed: () => controller.toggleFavorite(cocktail.idDrink),
-              );
-            }),
+              ],
+            ),
           ),
         ],
       ),
